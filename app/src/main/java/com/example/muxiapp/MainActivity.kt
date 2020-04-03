c176e95d0d822a8fcb9a28ac89dbbd5c7114f6d8
marcelolamenza <marcelolamenza013@gmail.com>
2020-04-03 13:18:42 -0300

    novo fragment

--- app/src/main/java/com/example/muxiapp/MainActivity.kt
+++ app/src/main/java/com/example/muxiapp/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.muxiapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.example.navigation.databinding.ActivityMainBinding
-
+import com.example.muxiapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
